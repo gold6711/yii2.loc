cfc842926ed47c13261af4e2e3e7dcc5a6b6919a
gold6711 <v_igor84@yahoo.com>
2016-10-13 23:43:59 +0300

    lesson 13 search function -second part -view

--- controllers/CategoryController.php
+++ controllers/CategoryController.php
@@ -30,7 +30,10 @@ class CategoryController extends AppController{
     }
 
     public function actionSearch(){
-        $q = Yii::$app->request->get('q');
+        $q = trim(Yii::$app->request->get('q'));
+        $this->setMeta('E-SHOPPER | Поиск: '. $q);
+        if(!$q)
+            return $this->render('search');
         $query = Product::find()->where(['like', 'name', $q]);
         $pages = new Pagination(['totalCount' => $query->count(), 'pageSize' => 3, 'forcePageParam' => false, 'pageSizeParam' => false]);
         $products = $query->offset($pages->offset)->limit($pages->limit)->all();
