dd18fa2aa224e0e5ee16d2c370ea92bb53e4c4bb
gold6711 <v_igor84@yahoo.com>
2016-10-15 02:08:48 +0300

    lesson 16 - Cart actionClar

--- web/js/main.js
+++ web/js/main.js
@@ -6,6 +6,25 @@ $('.catalog').dcAccordion({
 	speed: 300
 });
 
+function showCart(cart){
+	$('#cart .modal-body').html(cart);
+	$('#cart').modal();
+}
+
+function clearCart(){
+	$.ajax({
+		url: '/cart/clear',
+		type: 'GET',
+		success: function(res){
+			if(!res) alert('Ошибка!');
+			showCart(res);
+		},
+		error: function(){
+			alert('Error!');
+		}
+	});
+}
+
 $('.add-to-cart').on('click', function (e) {
 	e.preventDefault();
 	var id = $(this).data('id');
@@ -15,8 +34,8 @@ $('.add-to-cart').on('click', function (e) {
 		type: 'GET',
 		success: function(res){
 			if(!res) alert('Ошибка!');
-			console.log(res);
-			//showCart(res);
+			//console.log(res);
+			showCart(res);
 		},
 		error: function(){
 			alert('Error!');
