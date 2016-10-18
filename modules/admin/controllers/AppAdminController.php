dedeab8ac022850241b20e5eddbe9489e010d737
gold6711 <v_igor84@yahoo.com>
2016-10-18 22:44:19 +0300

    lesson 22 - authorization part 1

---
+++ modules/admin/controllers/AppAdminController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 18.10.2016
+ * Time: 21:57
+ */
+
+namespace app\modules\admin\controllers;
+use yii\web\Controller;
+use yii\filters\AccessControl;
+
+class AppAdminController extends Controller {
+
+    public function behaviors(){
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+}
