a2f61bad96bc7bfb274a4f5ab9c83b2e808dcf0b
gold6711 <v_igor84@yahoo.com>
2016-11-03 00:01:52 +0300

    lesson 29

--- modules/admin/views/product/_form.php
+++ modules/admin/views/product/_form.php
@@ -3,6 +3,8 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 use mihaildev\ckeditor\CKEditor;
+use mihaildev\elfinder\ElFinder;
+mihaildev\elfinder\Assets::noConflict($this);
 
 /* @var $this yii\web\View */
 /* @var $model app\modules\admin\models\Product */
@@ -23,12 +25,18 @@ checkbox
 
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
 
+<!--    --><?php
+//    echo $form->field($model, 'content')->widget(CKEditor::className(),[
+//    'editorOptions' => [
+//    'preset' => 'full', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
+//    'inline' => false, //по умолчанию false
+//    ],
+//    ]);
+//    ?>
+
     <?php
-    echo $form->field($model, 'content')->widget(CKEditor::className(),[
-    'editorOptions' => [
-    'preset' => 'full', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
-    'inline' => false, //по умолчанию false
-    ],
+    echo $form->field($model, 'content')->widget(CKEditor::className(), [
+    'editorOptions' => ElFinder::ckeditorOptions('elfinder',[]),
     ]);
     ?>
 
