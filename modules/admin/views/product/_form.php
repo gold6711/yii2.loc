23ed1301a04ebf60ac21ec427186f393ade23784
gold6711 <v_igor84@yahoo.com>
2016-11-04 21:18:13 +0300

    lesson 30 загрузка картинок часть 1

--- modules/admin/views/product/_form.php
+++ modules/admin/views/product/_form.php
@@ -13,7 +13,7 @@ mihaildev\elfinder\Assets::noConflict($this);
 checkbox
 <div class="product-form">
 
-    <?php $form = ActiveForm::begin(); ?>
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
 
     <div class="form-group field-product-category_id has-success">
         <label class="control-label" for="product-category_id">Родитеьская категория</label>
@@ -22,17 +22,7 @@ checkbox
         </select>
     </div>
 
-
     <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
-
-<!--    --><?php
-//    echo $form->field($model, 'content')->widget(CKEditor::className(),[
-//    'editorOptions' => [
-//    'preset' => 'full', //разработанны стандартные настройки basic, standard, full данную возможность не обязательно использовать
-//    'inline' => false, //по умолчанию false
-//    ],
-//    ]);
-//    ?>
 
     <?php
     echo $form->field($model, 'content')->widget(CKEditor::className(), [
@@ -46,7 +36,7 @@ checkbox
 
     <?= $form->field($model, 'description')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'img')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'image')->fileInput() ?>
 
     <?= $form->field($model, 'hit')->checkbox([ '0', '1', ], ['prompt' => '']) ?>
 
