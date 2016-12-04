a83a38bbbcf1e01198813f3d0e405b91ea29531a
gold6711 <v_igor84@yahoo.com>
2016-12-04 23:26:52 +0300

    redaction web.php file

--- config/web.php
+++ config/web.php
@@ -4,10 +4,7 @@ $params = require(__DIR__ . '/params.php');
 
 $config = [
     'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
-    'language' => 'ru',
-    //'layout' => 'basic', // изменение основного шаблона
+
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
